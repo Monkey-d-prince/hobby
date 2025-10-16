049e4a12a15c8fcd8503d646b988d118b8201395
Prince Gupta <129318983+Monkey-d-prince@users.noreply.github.com>
2025-10-16 20:51:23 +0530

    updated backend api to hosted one

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { User, GraphData, UserCreate } from '../types';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
